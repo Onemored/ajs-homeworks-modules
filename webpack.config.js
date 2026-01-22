aed0200c9337aaaad2fde63217c489f3ecc121b5
Sergey Utkin <bigduck@mail.ru>
2026-01-22 16:44:51 +0500

    Изменение по требованию преподавателя.

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,48 @@ const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-  },
+  return {
+    entry: './src/index.js',
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/i,
-        exclude: /node_modules/,
-        parser: { sourceType: 'module' },
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? 'bundle.[contenthash].js' : 'bundle.js',
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      ...(isProd
+        ? [
+          new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css',
+          }),
+        ]
+        : []),
     ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-    }),
-  ],
+    devServer: {
+      port: 8080,
+      hot: true,
+      open: true,
+      client: { overlay: true },
+    },
+  };
 };
